cbfce699e190ffc25be0493a49d7cbf7fc8d36ec
jerryclarkc0420 <jerryclarkc0420@gmail.com>
2023-03-19 16:50:02 +0800

    my-first-react-app

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import {useEffect, useState} from "react";
+
+import MovieCard from "./MovieCard";
+
+import './App.css';
+import SearchIcon from './search.svg'
+
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=44a065d9';
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        setMovies(data.Search)
+    }
+    
+    //useEffect hook will trigger a certain function when the component loads up. 
+    useEffect(() => {
+        searchMovies("resident evil")
+    }, []) //"[]" (empty dependency array) means that use effect will only run once
+
+    return (
+        <div className="app">
+            <h1>MovieSearch</h1>
+            <div className="search">
+                <input
+                    placeholder="Search for movies "
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img 
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? 
+                (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ) :
+                (
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )}
+
+        </div>
+    ); 
+}
+
+export default App;
