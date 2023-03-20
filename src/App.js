532075f301f857af9b4e4f8c95fb39c0d771d862
jerryclarkc0420 <jerryclarkc0420@gmail.com>
2023-03-20 09:43:31 +0800

    updated ui

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=44a065d9';
 
 const App = () => {
 
+    //"movies" is the state(or dynamic variable) and "setMovies" is the function that set the value for the "movies" variable. and [] is the initial value of the state
     const [movies, setMovies] = useState([]);
+
     const [searchTerm, setSearchTerm] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
 
     const searchMovies = async (title) => {
+        setIsLoading(true)
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
         setMovies(data.Search)
+        setIsLoading(false)
     }
     
     //useEffect hook will trigger a certain function when the component loads up. 
@@ -29,11 +34,18 @@ const App = () => {
     return (
         <div className="app">
             <h1>MovieSearch</h1>
+            <p className="sub-title">This simple react app uses the OMDB API to retrieve movie searches.</p>
+            
             <div className="search">
                 <input
                     placeholder="Search for movies "
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            searchMovies(searchTerm)
+                          }
+                    }}
                 />
                 <img 
                     src={SearchIcon}
@@ -41,22 +53,32 @@ const App = () => {
                     onClick={() => searchMovies(searchTerm)}
                 />
             </div>
-
-            {movies?.length > 0
-                ? 
-                (
-                    <div className="container">
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie} />
-                        ))}
-                    </div>
-                ) :
+                
+            {
+                isLoading ?
                 (
                     <div className="empty">
-                        <h2>No movies found</h2>
+                        <h2>Searching...</h2>
                     </div>
-                )}
-
+                ) :
+                (movies?.length > 0
+                    ? 
+                    (
+                        <div className="container">
+                            {movies.map((movie) => (
+                                <MovieCard movie={movie} />
+                            ))}
+                        </div>
+                    ) :
+                    (
+                        <div className="empty">
+                            <h2>No movies found</h2>
+                        </div>
+                    ))
+            }
+        <footer>
+        <p className="sub-title">Design credits to JavascriptMastery.</p>
+        </footer>
         </div>
     ); 
 }
